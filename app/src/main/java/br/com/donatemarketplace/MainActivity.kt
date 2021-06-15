b842ad8890ca4a2b468f042f37fb46756e82c15a
Jonathan Andrade <jonathanmaxuel.ti@gmail.com>
2021-06-14 21:39:06 -0300

    Creating project and adding navigation container in main activity

---
+++ app/src/main/java/br/com/donatemarketplace/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.donatemarketplace
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.main_activity)
+    }
+}
